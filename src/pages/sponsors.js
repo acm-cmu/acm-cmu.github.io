dc050b71d3bc511659fc62dc4b7e4bd0f36de5fb
Zanzao Chen <zanzaoc@andrew.cmu.edu>
2025-07-11 02:03:44 -0400

    absolute paths

--- src/pages/sponsors.js
+++ src/pages/sponsors.js
@@ -21,37 +21,37 @@ export default function Sponsors() {
 
         <div className="grid" id="logo-grid">
           <a className="sponsor-link white-bg" href="https://www.citadel.com" target="_blank" rel="noopener noreferrer">
-            <img src={`${process.env.PUBLIC_URL}/images/citadel.png`} alt="Citadel" style={{ width: "200px", height: "auto" }} />
+            <img src="https://www.acmatcmu.com/images/citadel.png" alt="Citadel" style={{ width: "200px", height: "auto" }} />
           </a>
           <a className="sponsor-link white-bg" href="https://www.thetradedesk.com/us" target="_blank" rel="noopener noreferrer">
-            <img src={`${process.env.PUBLIC_URL}/images/thetradedesk.png`} alt="The Trade Desk" style={{ width: "200px", height: "auto" }} />
+            <img src="https://www.acmatcmu.com/images/thetradedesk.png" alt="The Trade Desk" style={{ width: "200px", height: "auto" }} />
           </a>
           <a className="sponsor-link" href="https://www.hudsonrivertrading.com/" target="_blank" rel="noopener noreferrer">
-            <img src={`${process.env.PUBLIC_URL}/images/hrt.png`} alt="HRT" style={{ width: "200px", height: "auto" }} />
+            <img src="https://www.acmatcmu.com/images/hrt.png" alt="HRT" style={{ width: "200px", height: "auto" }} />
           </a>
           <a className="sponsor-link" href="https://stripe.com" target="_blank" rel="noopener noreferrer">
-            <img src={`${process.env.PUBLIC_URL}/images/stripe.png`} alt="Stripe" style={{ width: "200px", height: "auto" }} />
+            <img src="https://www.acmatcmu.com/images/stripe.png" alt="Stripe" style={{ width: "200px", height: "auto" }} />
           </a>
           <a className="sponsor-link white-bg" href="https://www.deshaw.com" target="_blank" rel="noopener noreferrer">
-            <img src={`${process.env.PUBLIC_URL}/images/deshaw.png`} alt="D. E. Shaw" style={{ width: "200px", height: "auto" }} />
+            <img src="https://www.acmatcmu.com/images/deshaw.png" alt="D. E. Shaw" style={{ width: "200px", height: "auto" }} />
           </a>
           <a className="sponsor-link white-bg" href="https://www.janestreet.com" target="_blank" rel="noopener noreferrer">
-            <img src={`${process.env.PUBLIC_URL}/images/janestreet.png`} alt="Jane Street" style={{ width: "200px", height: "auto" }} />
+            <img src="https://www.acmatcmu.com/images/janestreet.png" alt="Jane Street" style={{ width: "200px", height: "auto" }} />
           </a>
           <a className="sponsor-link white-bg" href="https://www.lockheedmartin.com" target="_blank" rel="noopener noreferrer">
-            <img src={`${process.env.PUBLIC_URL}/images/lockheed.png`} alt="Lockheed Martin" style={{ width: "200px", height: "auto" }} />
+            <img src="https://www.acmatcmu.com/images/lockheed.png" alt="Lockheed Martin" style={{ width: "200px", height: "auto" }} />
           </a>
           <a className="sponsor-link white-bg" href="https://www.anthropic.com" target="_blank" rel="noopener noreferrer">
-            <img src={`${process.env.PUBLIC_URL}/images/anthropic.png`} alt="Anthropic" style={{ width: "200px", height: "auto" }} />
+            <img src="https://www.acmatcmu.com/images/anthropic.png" alt="Anthropic" style={{ width: "200px", height: "auto" }} />
           </a>
           <a className="sponsor-link" href="https://ethereum.org" target="_blank" rel="noopener noreferrer">
-            <img src={`${process.env.PUBLIC_URL}/images/ethereum.png`} alt="Ethereum" style={{ width: "200px", height: "auto" }} />
+            <img src="https://www.acmatcmu.com/images/ethereum.png" alt="Ethereum" style={{ width: "200px", height: "auto" }} />
           </a>
           <a className="sponsor-link" href="https://www.sandia.gov" target="_blank" rel="noopener noreferrer">
-            <img src={`${process.env.PUBLIC_URL}/images/sandia.svg`} alt="Sandia" style={{ width: "200px", height: "auto" }} />
+            <img src="https://www.acmatcmu.com/images/sandia.svg" alt="Sandia" style={{ width: "200px", height: "auto" }} />
           </a>
           <a className="sponsor-link" href="https://www.scm-lp.com/" target="_blank" rel="noopener noreferrer">
-            <img src={`${process.env.PUBLIC_URL}/images/scm.png`} alt="SCM" style={{ width: "200px", height: "auto" }} />
+            <img src="https://www.acmatcmu.com/images/scm.png" alt="SCM" style={{ width: "200px", height: "auto" }} />
           </a>
         </div>
       </div>
